5e6e5da664fffdfc3683af9c3c5d3c6ce1074e51
vapuppet <victor.alvarez@puppet.com>
2022-09-06 12:19:35 +0000

    test

--- site-modules/profile/manifests/vsftpd/mod/vsftpdmod1.pp
+++ site-modules/profile/manifests/vsftpd/mod/vsftpdmod1.pp
@@ -1,10 +1,10 @@
 class profile::vsftpd::mod::vsftpdmod1 {
   include vsftpd
-}
 
-class { 'vsftpdmod1':
+class { 'vsftpd':
   anonymous_enable  => 'YES',
   write_enable      => 'YES',
   ftpd_banner       => 'Marmotte FTP Server Modified',
   chroot_local_user => 'YES',
+  }
 }
