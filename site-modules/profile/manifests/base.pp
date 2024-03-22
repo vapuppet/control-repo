d8c75922d0131295dc82fccdeba58a07b62cab5c
Victor Alvarez <91897997+vapuppet@users.noreply.github.com>
2024-03-22 13:22:01 -0400

    Update base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,15 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
-
+user { 'victor':
+  ensure             => 'present',
+  gid                => 1001,
+  home               => '/home/victor',
+  password           => '!',
+  password_max_age   => 99999,
+  password_min_age   => 0,
+  password_warn_days => 7,
+  provider           => 'useradd',
+  shell              => '/bin/sh',
+  uid                => 1001,
+}
 }
